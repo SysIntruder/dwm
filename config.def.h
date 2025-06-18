aa171446e422d72b4928a97b22ddc6ab0d486f72
rei <raihanryuichi@gmail.com>
2025-06-18 13:04:14 +0700

    adjust padding

--- config.def.h
+++ config.def.h
@@ -2,13 +2,13 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 16;       /* gaps between windows */
+static const unsigned int gappx     = 8;        /* gaps between windows */
 static const unsigned int snap      = 0;        /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 16;       /* vertical padding of bar */
+static const int vertpad            = 8;        /* vertical padding of bar */
 static const int sidepad            = 240;      /* horizontal padding of bar */
-static const int horizpadbar        = 4;        /* horizontal padding for statusbar */
+static const int horizpadbar        = 8;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 8;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "Terminus (TTF):size=10" };
 static const char col_gray1[]       = "#222222";
